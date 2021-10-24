a18f67786f3a4e74850137c8610e74562e47f0b1
Felipe Chagas <felip.uece@gmail.com>
2021-10-24 15:29:14 -0300

    :construction: Changing theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,74 @@
 import React, { Fragment } from 'react';
 import Head from 'next/head';
-import Sticky from 'react-stickynode';
 import { ThemeProvider } from 'styled-components';
-import { agencyTheme } from 'common/theme/agency';
+import { theme } from 'common/theme/appCreative';
 import ResetCSS from 'common/assets/css/style';
-import { GlobalStyle, AgencyWrapper } from 'containers/Agency/agency.style';
-import Navbar from 'containers/Agency/Navbar';
-import BannerSection from 'containers/Agency/BannerSection';
-import FeatureSection from 'containers/Agency/FeatureSection';
-import AboutUsSection from 'containers/Agency/AboutUsSection';
-import WorkHistory from 'containers/Agency/WorkHistory';
-import BlogSection from 'containers/Agency/BlogSection';
-import TestimonialSection from 'containers/Agency/TestimonialSection';
-import TeamSection from 'containers/Agency/TeamSection';
-import VideoSection from 'containers/Agency/VideoSection';
-import NewsletterSection from 'containers/Agency/NewsletterSection';
-import QualitySection from 'containers/Agency/QualitySection';
-import Footer from 'containers/Agency/Footer';
-import { DrawerProvider } from 'common/contexts/DrawerContext';
-import FaqSection from 'containers/Agency/FaqSection';
+import Sticky from 'react-stickynode';
+import Navbar from 'containers/AppCreative/Navbar';
+import Banner from 'containers/AppCreative/Banner';
+import KeyFeatures from 'containers/AppCreative/KeyFeatures';
+import Experiences from 'containers/AppCreative/Experience';
+import ChooseUs from 'containers/AppCreative/ChooseUs';
+import Pricing from 'containers/AppCreative/Pricing';
+import Testimonials from 'containers/AppCreative/Testimonials';
+import AvailableStore from 'containers/AppCreative/AvailableStore';
+import Faqs from 'containers/AppCreative/Faq';
+import CallToAction from 'containers/AppCreative/CallToAction';
+import FeatureSlider from 'containers/AppCreative/FeatureSlider';
+import Footer from 'containers/AppCreative/Footer';
+import GlobalStyle, {
+  AppWrapper,
+  ContentWrapper,
+} from 'containers/AppCreative/appCreative.style';
 
-const Main = () => {
+export default function () {
   return (
-    <ThemeProvider theme={agencyTheme}>
+    <ThemeProvider theme={theme}>
       <Fragment>
-        {/* Start agency head section */}
         <Head>
-          <title>Agency | A react next landing page</title>
-          <meta name="theme-color" content="#10ac84" />
+          <title>App Creative | A next js landing page</title>
           <meta name="Description" content="React next landing page" />
-          {/* Load google fonts */}
+          <meta name="theme-color" content="#6C247E" />
+          <meta
+            name="keywords"
+            content="React, React js, Next, Next js, Super fast next js landing, Creative landing, Next js landing"
+          />
           <link
-            href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans"
+            rel="stylesheet"
+          ></link>
         </Head>
+        {/* end of head */}
+
         <ResetCSS />
         <GlobalStyle />
-        {/* End of agency head section */}
-        {/* Start agency wrapper section */}
-        <AgencyWrapper>
-          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
-            <DrawerProvider>
-              <Navbar />
-            </DrawerProvider>
+        {/* end of global and reset style */}
+
+        {/* start app creative landing */}
+        <AppWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-active">
+            <Navbar />
           </Sticky>
-          <BannerSection />
-          <FeatureSection />
-          <AboutUsSection />
-          <WorkHistory />
-          <BlogSection />
-          <QualitySection />
-          <VideoSection />
-          <TestimonialSection />
-          <TeamSection />
-          <FaqSection />
-          <NewsletterSection />
+          <ContentWrapper>
+            <Banner />
+            <KeyFeatures />
+            <ChooseUs />
+            <Experiences />
+            <FeatureSlider />
+            <Pricing />
+            <Testimonials />
+            <AvailableStore />
+            <Faqs />
+            <CallToAction />
+          </ContentWrapper>
           <Footer />
-        </AgencyWrapper>
-        {/* End of agency wrapper section */}
+        </AppWrapper>
+        {/* end of app creative landing */}
       </Fragment>
     </ThemeProvider>
   );
-};
-export default Main;
+}
